1d52c34e0a5c1bcb32c3ec86ee3d788d95ea1d19
Ethan Albers <ekalbers22@gmail.com>
2023-05-11 01:10:43 -0500

    update README and test

---
+++ README.md
@@ -0,0 +1,17 @@
+# LAB - Class 27
+## Project: Django Models
+## Author: Ethan Albers
+## Links/Resources
+- [Github Repo](https://github.com/ekalbers/django-models)
+
+## setup
+- `python3 -m venv .venv`
+- `source .venv/bin/activate`
+- `pip install -r requirements.txt`
+- `npm install`
+
+## run application
+- `python manage.py runserver`
+
+## tests
+- `python manage.py test`
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SnacksTests(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_snack_list(self):
+        url = reverse('snacks_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_snack_list_template(self):
+        url = reverse('snacks_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snacks_list.html')
+        self.assertTemplateUsed(response, 'base.html')
